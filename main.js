6de72d0bd8a363ba86e1dd4ff63a45d7f6e03f2b
Saad Elnady <saad.elnady.work@gmail.com>
2023-05-10 14:52:29 +0300

    create function remove disabled

--- main.js
+++ main.js
@@ -3,65 +3,79 @@ let previousBtn = document.getElementById("previous");
 let btns = document.querySelectorAll(".buttons button");
 
 function removeClassActive() {
-  btns.forEach((btn) => {
-    btn.classList.remove("active");
-  });
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
 }
 let i = 1;
 
 function nextUser() {
-  if (i < 10) {
-    i++;
-    getUsers();
-    getPosts(i);
-    removeClassActive();
-    nextBtn.classList.add("active");
-  } else {
-    nextBtn.classList.add("disabled");
-    return false;
-  }
+    removeDisabled();
+    if (i < 10) {
+        i++;
+        getUsers();
+        getPosts(i);
+        removeClassActive();
+        nextBtn.classList.add("active");
+    } else {
+        nextBtn.classList.add("disabled");
+        return false;
+    }
 }
 
 function previousUser() {
-  if (i > 1) {
-    i--;
-    getUsers();
-    getPosts(i);
-    removeClassActive();
-    previousBtn.classList.add("active");
-  } else {
-    previousBtn.classList.add("disabled");
-    return false;
-  }
+    removeDisabled();
+    if (i > 1) {
+        i--;
+        getUsers();
+        getPosts(i);
+        removeClassActive();
+    } else {
+        previousBtn.classList.add("disabled");
+        return false;
+    }
+}
+
+function removeDisabled() {
+    if (i === 1) {
+        previousBtn.classList.add("disabled");
+    } else {
+        previousBtn.classList.remove("disabled");
+    }
+    if (i === 10) {
+        nextBtn.classList.add("disabled");
+    } else {
+        next.classList.remove("disabled");
+    }
 }
 
 function getResponse(url) {
-  return new Promise((resolve) => {
-    fetch(url).then((response) => {
-      resolve(response.json());
+    return new Promise((resolve) => {
+        fetch(url).then((response) => {
+            resolve(response.json());
+        });
     });
-  });
 }
 
 function getUsers() {
-  getResponse(`https://jsonplaceholder.typicode.com/users/${i}`)
-    .then((data) => {
-      let Name = data.name;
-      let userName = data.username;
-      let userEmail = data.email;
-      let userAddressStreet = data.address.street;
-      let userAddressSuite = data.address.suite;
-      let city = data.address.city;
-      let zipcode = data.address.zipcode;
-      let phoneNumber = data.phone;
-      let webSite = data.website;
-      let companyName = data.company.name;
-      let companyPhrase = data.company.catchPhrase;
-      let companyBs = data.company.bs;
+    getResponse(`https://jsonplaceholder.typicode.com/users/${i}`)
+        .then((data) => {
+            let Name = data.name;
+            let userName = data.username;
+            let userEmail = data.email;
+            let userAddressStreet = data.address.street;
+            let userAddressSuite = data.address.suite;
+            let city = data.address.city;
+            let zipcode = data.address.zipcode;
+            let phoneNumber = data.phone;
+            let webSite = data.website;
+            let companyName = data.company.name;
+            let companyPhrase = data.company.catchPhrase;
+            let companyBs = data.company.bs;
 
-      let card = document.querySelector(".card");
+            let card = document.querySelector(".card");
 
-      card.innerHTML = `
+            card.innerHTML = `
                         <h2 class="name"> ${Name}</h2>
                         <p class="userName"><span>userName:</span>  ${userName}</p>
                         <p class="userEmail"><span>Email :</span>   ${userEmail}</p>
@@ -75,10 +89,10 @@ function getUsers() {
                         <p class="zipcode"> <span>phrase:</span> ${companyPhrase}</p>
                         <p class="zipcode"> <span>bs:</span> ${companyBs}</p>
         `;
-    })
+        })
 
     .catch((error) => {
-      console.log(error);
+        console.log(error);
     });
 }
 
@@ -107,29 +121,28 @@ function getUsers() {
 // }
 
 function getPosts(user_id) {
-  getResponse(`https://jsonplaceholder.typicode.com/posts`).then((data) => {
-    let posts = document.querySelector(".posts");
+    getResponse(`https://jsonplaceholder.typicode.com/posts`).then((data) => {
+        let posts = document.querySelector(".posts");
 
-    const allPosts = [...data];
+        const allPosts = [...data];
 
-    const filteredData = allPosts.filter((item) => item.userId === user_id);
+        const filteredData = allPosts.filter((item) => item.userId === user_id);
 
-    let singlePostWrapper = "";
-    filteredData.map((post) => {
-      console.log(post);
-      let postId = post.id;
-      let postTitle = post.title;
-      let postBody = post.body;
-      singlePostWrapper += `
+        let singlePostWrapper = "";
+        filteredData.map((post) => {
+            let postId = post.id;
+            let postTitle = post.title;
+            let postBody = post.body;
+            singlePostWrapper += `
                 <div class="post">
                     <span>${postId}</span>
                     <h3 class="title">${postTitle}</h3>
                     <p class="postContent">${postBody}</p>
                 </div >
                 `;
+        });
+        posts.innerHTML = singlePostWrapper;
     });
-    posts.innerHTML = singlePostWrapper;
-  });
 }
 getUsers();
 getPosts(i);
